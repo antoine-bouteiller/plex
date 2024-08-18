f803ea41f5c363e08c05008bfa1f6260a4086541
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2024-08-18 17:51:40 +0200

    feat(sonarr): remove 4k

--- scripts/autolanguageconfig/app/server.ts
+++ scripts/autolanguageconfig/app/server.ts
@@ -3,7 +3,6 @@ import { logger } from "../config/logger";
 import { updateLanguage } from "./services/updateLanguage";
 
 const job = new CronJob("0 */5 * * * *", async () => {
-  logger.info("Starting update tv shows and movies language");
   await updateLanguage(1);
   await updateLanguage(2);
 });
