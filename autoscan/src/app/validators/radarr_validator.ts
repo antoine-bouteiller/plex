56865105b9cdf73c0c7eb59a3f89368ea2f89c26
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2025-07-26 21:45:28 +0200

    fix: webhook validators

--- autoscan/src/app/validators/radarr_validator.ts
+++ autoscan/src/app/validators/radarr_validator.ts
@@ -2,12 +2,16 @@ import { z } from 'zod/v4'
 
 export const radarrValidator = z.object({
   eventType: z.enum(['DeleteFile', 'Download', 'Test']),
-  movie: z.object({
-    tmdbId: z.coerce.number(),
-    folderPath: z.string(),
-    title: z.string(),
-  }),
-  movieFile: z.object({
-    relativePath: z.string(),
-  }),
+  movie: z.optional(
+    z.object({
+      tmdbId: z.coerce.number(),
+      folderPath: z.string(),
+      title: z.string(),
+    })
+  ),
+  movieFile: z.optional(
+    z.object({
+      relativePath: z.string(),
+    })
+  ),
 })
--- autoscan/src/app/validators/sonarr_validator.ts
+++ autoscan/src/app/validators/sonarr_validator.ts
@@ -1,18 +1,24 @@
 import { z } from 'zod/v4'
 
 export const sonarrValidator = z.object({
-  episodeFile: z.object({
-    relativePath: z.string(),
-  }),
-  episodes: z.array(
+  episodeFile: z.optional(
     z.object({
+      relativePath: z.string(),
+    })
+  ),
+  episodes: z.optional(
+    z.array(
+      z.object({
+        title: z.string(),
+      })
+    )
+  ),
+  eventType: z.enum(['Download', 'EpisodeFileDelete', 'SeriesDelete', 'Test']),
+  series: z.optional(
+    z.object({
+      tmdbId: z.coerce.number(),
+      path: z.string(),
       title: z.string(),
     })
   ),
-  eventType: z.enum(['Download', 'EpisodeFileDelete', 'SeriesDelete', 'Test']),
-  series: z.object({
-    tmdbId: z.coerce.number(),
-    path: z.string(),
-    title: z.string(),
-  }),
 })
