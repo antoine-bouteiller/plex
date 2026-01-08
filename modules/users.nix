d98c658f6b818a7f71a94089fb6b45424ecfbd19
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-01-08 07:38:13 +0100

    feat(ssh): add pelico maco to authried keys

--- modules/users.nix
+++ modules/users.nix
@@ -6,6 +6,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICUZ+rb5WpsUv/4wVWlQ0aCRiNzZCIQngxXiNAJx6hJs antob@DESKTOP-ANTOINE"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhSmVRhMXbxDkDOaUk0udibjBos2nDg6byvZ//dzMwL antob@DESKTOP-3R8RBJU"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfK4sKI0QpEbaADjQm/7bK3DlNY/akOh+6yC+q3aG17 antoine.bouteiller@pelico.io"
     ];
   };
 
