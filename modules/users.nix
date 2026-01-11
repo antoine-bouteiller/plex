9529ff334fadeb66a7d9480b07386c2f3c46e4a5
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-01-11 16:06:04 +0100

    feat: add user to podman group

--- modules/users.nix
+++ modules/users.nix
@@ -2,7 +2,7 @@
   users.users.antoineb = {
     isNormalUser = true;
     description = "Antoine Bouteiller";
-    extraGroups = ["networkmanager" "wheel" "docker" "media"];
+    extraGroups = ["networkmanager" "wheel" "podman" "media"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICUZ+rb5WpsUv/4wVWlQ0aCRiNzZCIQngxXiNAJx6hJs antob@DESKTOP-ANTOINE"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhSmVRhMXbxDkDOaUk0udibjBos2nDg6byvZ//dzMwL antob@DESKTOP-3R8RBJU"
