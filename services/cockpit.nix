ac8b89057eae33ebe68514e15bdbb1dd037d76a2
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-01-26 16:00:19 +0100

    fix: update cockpit configuration

--- services/cockpit.nix
+++ services/cockpit.nix
@@ -1,17 +1,30 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   port = config.server.ports.cockpit;
+  domain = "cockpit.${config.server.network.domain}";
 in {
   services.cockpit = {
     enable = true;
     inherit port;
+
+    settings = {
+      WebService = {
+        AllowUnencrypted = true;
+        Origins = lib.mkForce "https://${domain}";
+        ProtocolHeader = "X-Forwarded-Proto";
+      };
+    };
   };
 
-  services.caddy.virtualHosts."cockpit.${config.server.network.domain}" = {
+  services.caddy.virtualHosts."${domain}" = {
     extraConfig = ''
+      @cockpit_ws path /cockpit/socket/*
+      reverse_proxy @cockpit_ws localhost:${toString port}
+
       import auth_proxy
       reverse_proxy localhost:${toString port}
     '';
