33cfcb8cd832483f99e390c703d5e0d70dd374bb
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-01-27 15:22:50 +0100

    fix: add flake path to update module

--- modules/update.nix
+++ modules/update.nix
@@ -1,20 +1,40 @@
-{
-  pkgs,
-  ...
-}:{
+{pkgs, ...}: let
+  flakePath = "/etc/nixos";
+in {
   systemd.services = {
     flake-update = {
+      stopIfChanged = false;
+      restartIfChanged = false;
+
       unitConfig = {
         Description = "Update flake inputs";
         StartLimitIntervalSec = 300;
         StartLimitBurst = 5;
       };
+
       serviceConfig = {
-        ExecStart = "${pkgs.nix}/bin/nix flake update --commit-lock-file ${flakePath}";
+        WorkingDirectory = flakePath;
+
+        ExecStart = pkgs.writeShellScript "flake-update-script" ''
+          ${pkgs.nix}/bin/nix flake update
+
+          if ! ${pkgs.git}/bin/git diff --exit-code flake.lock > /dev/null; then
+              ${pkgs.git}/bin/git add flake.lock
+              ${pkgs.git}/bin/git commit -m "chore(deps): auto-update flake.lock"
+          fi
+        '';
         Restart = "on-failure";
         RestartSec = "30";
         Type = "oneshot";
+
+        Environment = [
+          "GIT_AUTHOR_NAME='Antoine Bouteiller'"
+          "GIT_AUTHOR_EMAIL=115460763+antoine-bouteiller@users.noreply.github.com"
+          "GIT_COMMITTER_NAME='Antoine Bouteiller'"
+          "GIT_COMMITTER_EMAIL=115460763+antoine-bouteiller@users.noreply.github.com"
+        ];
       };
+
       before = ["nixos-upgrade.service"];
       path = [pkgs.nix pkgs.git pkgs.host];
     };
@@ -22,12 +42,12 @@
 
   system.autoUpgrade = {
     enable = true;
-    dates = "01:00";
+    dates = "Mon *-*-* 01:00:00";
 
-    flake = "/etc/nixos";
+    flake = flakePath;
     flags = [
-       "-L"
-     ];
+      "-L"
+    ];
 
     allowReboot = true;
     rebootWindow = {
