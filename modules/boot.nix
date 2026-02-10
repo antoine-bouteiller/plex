3c5be478660e6abce0bc9e79948ec2061559c9be
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-02-10 21:36:38 +0100

    fix(boot): use pci reboot

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,6 +2,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = ["coretemp" "nct6775"];
+  boot.kernelParams = ["reboot=pci"];
 
   environment = {
     etc = {
