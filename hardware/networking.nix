b442410e4606638c65ff27b6407cae346ae48340
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-03-10 11:40:24 +0100

    feat(plex): use reverse proxy for connexion

--- hardware/networking.nix
+++ hardware/networking.nix
@@ -1,4 +1,4 @@
-{globals, ...}: {
+_: {
   networking = {
     hostName = "plex-server";
     nameservers = ["1.1.1.1" "9.9.9.9"];
--- services/plex.nix
+++ services/plex.nix
@@ -2,11 +2,17 @@
   services.plex = {
     enable = true;
     dataDir = globals.plex.dataDir;
-    openFirewall = true;
   };
 
   services.caddy.virtualHosts."plex.${globals.network.domain}" = {
-    extraConfig = "reverse_proxy localhost:${toString globals.plex.port}";
+    extraConfig = ''
+      reverse_proxy localhost:${toString globals.plex.port} {
+          header_up Host {host}
+          header_up X-Real-IP {remote_host}
+          header_up X-Forwarded-For {remote_host}
+          header_up X-Forwarded-Proto {scheme}
+      }
+    '';
   };
 
   users.users.plex.extraGroups = [globals.libraryOwner.group];
